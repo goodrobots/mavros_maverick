135e05f01514a30b47ef1bc92b6b58db7cddc6f5
Gregoire Linard <gregoire.linard@azurdrones.com>
2018-03-06 20:13:31 +0100

    Fix vehicle_info fields

--- src/plugins/vehicle_info.cpp
+++ src/plugins/vehicle_info.cpp
@@ -56,19 +56,19 @@ private:
 
 	ros::ServiceServer vehicle_info_srv;
 
-	MAV_AUTOPILOT autopilot;
-	MAV_TYPE type;
+	uint8_t autopilot = 0; //MAV_AUTOPILOT autopilot;
+	uint8_t type = 0; //MAV_TYPE type;
 	std::string mode;
-    uint32_t mode_id;
-	MAV_STATE system_status;
-    MAV_PROTOCOL_CAPABILITY capabilities;
-    uint32_t flight_sw_version;
-    uint32_t middleware_sw_version;
-    uint32_t os_sw_version;
-    uint32_t board_version;
-    uint16_t vendor_id;
-    uint16_t product_id;
-    uint64_t uid;
+    uint32_t mode_id = 0;
+	uint8_t system_status = 0; //MAV_STATE system_status;
+    uint64_t capabilities = 0; //MAV_PROTOCOL_CAPABILITY capabilities;
+    uint32_t flight_sw_version = 0;
+    uint32_t middleware_sw_version = 0;
+    uint32_t os_sw_version = 0;
+    uint32_t board_version = 0;
+    uint16_t vendor_id = 0;
+    uint16_t product_id = 0;
+    uint64_t uid = 0;
 
 
 	/* -*- message handlers -*- */
@@ -76,14 +76,14 @@ private:
 	{
 		using mavlink::common::MAV_MODE_FLAG;
 
-		if (!m_uas->is_my_target(msg->sysid)) {
+		if (m_uas->is_my_target(msg->sysid)) {
 			return;
 		}
 		      
-        type = static_cast<MAV_TYPE>(hb.type);
-		autopilot = static_cast<MAV_AUTOPILOT>(hb.autopilot);
-		mode = m_uas->str_mode_v10(hb.base_mode, hb.custom_mode);
-		system_status = static_cast<MAV_STATE>(hb.system_status);
+        type = hb.type; 
+		autopilot = hb.autopilot;
+		mode = m_uas->str_mode_v10(hb.base_mode, hb.custom_mode); //BUG ?
+		system_status = hb.system_status;
 
         if (!(hb.base_mode & enum_value(MAV_MODE_FLAG::CUSTOM_MODE_ENABLED))){
             mode_id = hb.base_mode;
@@ -95,12 +95,11 @@ private:
 
 	void handle_autopilot_version(const mavlink::mavlink_message_t *msg, mavlink::common::msg::AUTOPILOT_VERSION &apv)
 	{
-		
-        if (m_uas->is_my_target(msg->sysid, msg->compid)) {
-
-            process_autopilot_version_normal(apv, msg->sysid, msg->compid);
-            
-        }
+        if (m_uas->is_my_target(msg->sysid)) {
+			return;
+		}
+           
+        process_autopilot_version_normal(apv, msg->sysid, msg->compid);
 	}
 
 	/* -*-ROS callbacks -*- */
@@ -108,15 +107,15 @@ private:
 	 bool vehicle_info_cb(mavros_maverick::VehicleInfo::Request &req,
 			mavros_maverick::VehicleInfo::Response &res)
 	{
-        res.type =  enum_value(type);
-        res.autopilot = enum_value(autopilot); 
+        res.type =  type; 
+        res.autopilot = autopilot; 
         res.mode = mode;
         res.mode_id = mode_id;
-        res.system_status = enum_value(system_status);
+        res.system_status = system_status;
         res.sysid = m_uas->get_tgt_system();
 		res.compid = m_uas->get_tgt_component();
 
-        res.capabilities = enum_value(capabilities);
+        res.capabilities = capabilities;
         res.flight_sw_version = flight_sw_version;
         res.middleware_sw_version = middleware_sw_version;
         res.os_sw_version = os_sw_version;
@@ -133,18 +132,19 @@ private:
     void process_autopilot_version_normal(mavlink::common::msg::AUTOPILOT_VERSION &apv, uint8_t sysid, uint8_t compid)
 	{
 
-
-		capabilities = static_cast<MAV_PROTOCOL_CAPABILITY>(apv.capabilities);
-		flight_sw_version = apv.flight_sw_version;
-		// custom_version_to_hex_string(apv.flight_custom_version).c_str());
-		middleware_sw_version = apv.middleware_sw_version;
-		//custom_version_to_hex_string(apv.middleware_custom_version).c_str());
-		os_sw_version = apv.os_sw_version;
-		//custom_version_to_hex_string(apv.os_custom_version).c_str());
-		board_version = apv.board_version;
-		vendor_id = apv.vendor_id;
-        product_id = apv.product_id;
-		uid = apv.uid;
+        
+            capabilities = apv.capabilities; 
+            flight_sw_version = apv.flight_sw_version;
+            // custom_version_to_hex_string(apv.flight_custom_version).c_str());
+            middleware_sw_version = apv.middleware_sw_version;
+            //custom_version_to_hex_string(apv.middleware_custom_version).c_str());
+            os_sw_version = apv.os_sw_version;
+            //custom_version_to_hex_string(apv.os_custom_version).c_str());
+            board_version = apv.board_version;
+            vendor_id = apv.vendor_id;
+            product_id = apv.product_id;
+            uid = apv.uid;
+        
 	}
 
 	
