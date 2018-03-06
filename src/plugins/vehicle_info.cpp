6ae276be0d9d9a0f816d54bff5d1c820a7faa747
Gregoire Linard <gregoire.linard@azurdrones.com>
2018-03-07 00:47:21 +0100

    Debug msg

--- src/plugins/vehicle_info.cpp
+++ src/plugins/vehicle_info.cpp
@@ -58,6 +58,8 @@ private:
 
 	uint8_t autopilot = 0; //MAV_AUTOPILOT autopilot;
 	uint8_t type = 0; //MAV_TYPE type;
+    uint8_t sysid = 0;
+    uint8_t compid = 0;
 	std::string mode;
     uint32_t mode_id = 0;
 	uint8_t system_status = 0; //MAV_STATE system_status;
@@ -75,13 +77,18 @@ private:
 	void handle_heartbeat(const mavlink::mavlink_message_t *msg, mavlink::common::msg::HEARTBEAT &hb)
 	{
 		using mavlink::common::MAV_MODE_FLAG;
+        ROS_INFO_STREAM_NAMED("DEBUG", "DEBUG_V_INFO::handle_heartbeat: " << hb.to_yaml());
 
 		if (m_uas->is_my_target(msg->sysid)) {
-			return;
+			ROS_WARN("VEH_INFO : NOT MY TARGET");
+            return;
 		}
 		      
         type = hb.type; 
 		autopilot = hb.autopilot;
+        sysid = msg->sysid;
+        compid = msg->compid;
+
 		mode = m_uas->str_mode_v10(hb.base_mode, hb.custom_mode); //BUG ?
 		system_status = hb.system_status;
 
@@ -112,8 +119,8 @@ private:
         res.mode = mode;
         res.mode_id = mode_id;
         res.system_status = system_status;
-        res.sysid = m_uas->get_tgt_system();
-		res.compid = m_uas->get_tgt_component();
+        res.sysid = sysid; //m_uas->get_tgt_system();
+		res.compid = compid; //m_uas->get_tgt_component();
 
         res.capabilities = capabilities;
         res.flight_sw_version = flight_sw_version;
@@ -133,7 +140,7 @@ private:
     void process_autopilot_version_normal(mavlink::common::msg::AUTOPILOT_VERSION &apv, uint8_t sysid, uint8_t compid)
 	{
 
-        
+            ROS_INFO("DEBUG : GOT AUTOPILOT_VERSION MSG ");
             capabilities = apv.capabilities; 
             flight_sw_version = apv.flight_sw_version;
             // custom_version_to_hex_string(apv.flight_custom_version).c_str());
